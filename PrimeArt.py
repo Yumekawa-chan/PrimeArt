ff5437ac14a587c9a3888ae2ad5f2366021efc50
Yumekawa_chan <82374688+Yumekawa-chan@users.noreply.github.com>
2022-02-23 17:43:08 +0900

    Add files via upload

---
+++ PrimeArt.py
@@ -0,0 +1,22 @@
+import turtle
+
+turtle.shape('turtle')
+def prime(s):
+     flag = False
+     for i in range(2,s):
+          num = s % i
+          if num == 0:
+               flag = True
+               break
+     if flag == False:
+          return 1
+     else:
+          return 0
+
+num = 2
+while True:
+     turtle.forward(5)
+     if prime(num) == 1:
+          turtle.right(num)
+          print(num)
+     num += 1
